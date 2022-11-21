d3fcefc254138a29c69851eef56db4b6da0b9cbf
Michael Buck <mrmabuck@gmail.com>
2022-11-21 09:20:18 -0800

    ip adjustments

--- Communication/serverTest.py
+++ Communication/serverTest.py
@@ -8,7 +8,7 @@ import socket, cv2, pickle,struct,imutils
 # Socket Create
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
+host_ip = '164.67.207.174' #socket.gethostbyname(host_name)
 print('HOST IP:',host_ip)
 port = 9999
 socket_address = (host_ip,port)
