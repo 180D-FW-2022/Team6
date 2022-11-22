1a923d213a9f995e2092f7d1e6fa8676b9d036c9
Michael Buck <mrmabuck@gmail.com>
2022-11-21 18:52:57 -0800

    remove cv2)

--- Communication/serverTest.py
+++ Communication/serverTest.py
@@ -39,6 +39,6 @@ while True:
 			client_socket.sendall(message)
 			
 			# cv2.imshow('TRANSMITTING VIDEO',frame)
-			key = cv2.waitKey(1) & 0xFF
-			if key ==ord('q'):
-				client_socket.close()			
+			# key = cv2.waitKey(1) & 0xFF
+			# if key ==ord('q'):
+			# 	client_socket.close()			
