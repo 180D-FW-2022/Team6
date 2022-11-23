ac82edf7ed2ba747c7c94706b384ea8800d2e476
ovadiatal <tovadia@g.ucla.edu>
2022-11-22 22:29:13 -0800

    use vc to stop camera feed of pi
    also attempted to record and store video feed but files are corrupted

--- Speech Control/client_vc.py
+++ Speech Control/client_vc.py
@@ -1,6 +1,13 @@
 #Source: https://pyshine.com/Socket-programming-and-openc/
 # lets make the client code
 import socket,cv2, pickle,struct
+import speech_recognition as sr
+import cv2
+import numpy as np
+import threading
+import time
+import os
+
 
 # create socket
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -9,23 +16,78 @@ port = 9999
 client_socket.connect((host_ip,port)) # a tuple
 data = b""
 payload_size = struct.calcsize("Q")
-while True:
-	while len(data) < payload_size:
-		packet = client_socket.recv(4*1024) # 4K
-		if not packet: break
-		data+=packet
-	packed_msg_size = data[:payload_size]
-	data = data[payload_size:]
-	print(packed_msg_size)
-	msg_size = struct.unpack("Q",packed_msg_size)[0]
-	
-	while len(data) < msg_size:
-		data += client_socket.recv(4*1024)
-	frame_data = data[:msg_size]
-	data  = data[msg_size:]
-	frame = pickle.loads(frame_data)
-	cv2.imshow("RECEIVING VIDEO",frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key  == ord('q'):
-		break
+command = "m"
+def frompi():
+    global command
+    global data
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket.recv(4*1024) # 4K
+            if not packet: break
+            data+=packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        #print(packed_msg_size)
+        msg_size = struct.unpack("Q",packed_msg_size)[0]
+        
+        while len(data) < msg_size:
+            data += client_socket.recv(4*1024)
+        frame_data = data[:msg_size]
+        data  = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("RECEIVING VIDEO",frame)
+
+        ####
+        
+        if "stop" in command:
+            print("stop camera")
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+        
+        ####
+        key = cv2.waitKey(1) & 0xFF
+        if key  == ord('q'):
+            break
+
+
+def hear():
+    time.sleep(10)
+    
+    while(True):
+        
+        global command
+
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+
+        try:
+            command = r.recognize_google(audio)
+            print("Google Speech Recognition thinks you said " + command)
+
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+        time.sleep(3)
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=hear)
+    t2 = threading.Thread(target=frompi)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+
+
 client_socket.close()
+
+
