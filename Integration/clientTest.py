d9e5c92b126364a123665b2f4429ab65e3232058
Michael Buck <mrmabuck@gmail.com>
2022-11-23 17:04:36 -0800

    functional integration of communication, gesture recognition, and speech recognition

--- Integration/clientTest.py
+++ Integration/clientTest.py
@@ -3,6 +3,7 @@
 import socket,cv2, pickle,struct
 
 ######################### Speech Recognition Dependencies ##################################
+import speech_recognition as sr
 import threading
 import time
 import os
@@ -58,63 +59,63 @@ def frompi():
 		
 		while len(data) < msg_size:
 			data += client_socket.recv(4*1024)
-			frame_data = data[:msg_size]
-			data  = data[msg_size:]
-			frame = pickle.loads(frame_data)
+		frame_data = data[:msg_size]
+		data  = data[msg_size:]
+		frame = pickle.loads(frame_data)
 
-			cv2.imshow("RECEIVING VIDEO",frame)
+		cv2.imshow("RECEIVING VIDEO",frame)
 
-			################################ Gesture Recognition Code #########################################
-			x, y, c = frame.shape
+		################################ Gesture Recognition Code #########################################
+		x, y, c = frame.shape
 
-			# Flip the frame vertically
-			frame = cv2.flip(frame, 1)
-			framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		# Flip the frame vertically
+		frame = cv2.flip(frame, 1)
+		framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-			# Get hand landmark prediction
-			result = hands.process(framergb)
+		# Get hand landmark prediction
+		result = hands.process(framergb)
 
-			# print(result)
-			className = ''
+		# print(result)
+		className = ''
 
-			# post process the result
-			if result.multi_hand_landmarks:
-				landmarks = []
-				for handslms in result.multi_hand_landmarks:
-					for lm in handslms.landmark:
-						# print(id, lm)
-						lmx = int(lm.x * x)
-						lmy = int(lm.y * y)
-						
-						landmarks.append([lmx, lmy])
+		# post process the result
+		if result.multi_hand_landmarks:
+			landmarks = []
+			for handslms in result.multi_hand_landmarks:
+				for lm in handslms.landmark:
+					# print(id, lm)
+					lmx = int(lm.x * x)
+					lmy = int(lm.y * y)
+					
+					landmarks.append([lmx, lmy])
 
-					# Drawing landmarks on frames
-					mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+				# Drawing landmarks on frames
+				mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
 
-					# Predict gesture
-					prediction = model.predict([landmarks])
-					# print(prediction)
-					classID = np.argmax(prediction)
-					className = classNames[classID]
+				# Predict gesture
+				prediction = model.predict([landmarks])
+				# print(prediction)
+				classID = np.argmax(prediction)
+				className = classNames[classID]
 
-			# show the prediction on the frame
-			cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2, cv2.LINE_AA)
+		# show the prediction on the frame
+		cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2, cv2.LINE_AA)
 
-			# Show the final output
-			cv2.imshow("Output", frame)
+		# Show the final output
+		cv2.imshow("Output", frame)
 
-			######################################################################################################
+		######################################################################################################
 
-			#################################### Speech Recognition ###################################
-			if "stop" in command.lower():
-				print("stop camera")
-				cap.release()
-				cv2.destroyAllWindows()
-				break
-			###########################################################################################
-			
-			if cv2.waitKey(1) == ord('q'):
-				break
+		#################################### Speech Recognition ###################################
+		if "stop" in command.lower():
+			print("stop camera")
+			cap.release()
+			cv2.destroyAllWindows()
+			break
+		###########################################################################################
+		
+		if cv2.waitKey(1) == ord('q'):
+			break
 
 ############################################ Speech Recognition #############################################	
 def hear():
