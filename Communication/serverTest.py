1080c7021b11f981a43ee38aa34f05444c3b2b1b
Michael Buck <mrmabuck@gmail.com>
2023-01-12 17:01:33 -0800

    update ip

--- Communication/serverTest.py
+++ Communication/serverTest.py
@@ -10,7 +10,7 @@ import numpy as np
 # Socket Create
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name  = socket.gethostname()
-host_ip = '164.67.207.180' #socket.gethostbyname(host_name)
+host_ip = '169.232.126.138' #socket.gethostbyname(host_name)
 print('HOST IP:',host_ip)
 port = 9999
 socket_address = (host_ip,port)
