e58cad44d89c510ddeeb2504497e559a4ad92f2e
Michael Buck <mrmabuck@gmail.com>
2023-01-18 12:44:40 -0800

    Code cleanup

--- Communication/serverTest.py
+++ Communication/serverTest.py
@@ -12,11 +12,7 @@ import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from threading import Thread
-# from __future__ import print_function
 from imutils.video.pivideostream import PiVideoStream
-from imutils.video import FPS
-import argparse
-import time
 
 class PiVideoStream:
 	def __init__(self, resolution=(320, 240), framerate=32):
@@ -58,46 +54,8 @@ class PiVideoStream:
 		# indicate that the thread should be stopped
 		self.stopped = True
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-	help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-	help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
-# initialize the camera and stream
-# camera = PiCamera()
-# camera.resolution = (320, 240)
-# camera.framerate = 32
-# rawCapture = PiRGBArray(camera, size=(320, 240))
-# stream = camera.capture_continuous(rawCapture, format="bgr",
-# 	use_video_port=True)
-
-# created a *threaded *video stream, allow the camera sensor to warmup,
-# and start the FPS counter
 print("[INFO] sampling THREADED frames from `picamera` module...")
 vs = PiVideoStream().start()
-# time.sleep(2.0)
-# fps = FPS().start()
-# loop over some frames...this time using the threaded stream
-# while True:
-# 	# grab the frame from the threaded video stream and resize it
-# 	# to have a maximum width of 400 pixels
-# 	frame = vs.read()
-# 	# frame = imutils.resize(frame, width=320,inter=cv2.INTER_LANCZOS4)
-# 	# check to see if the frame should be displayed to our screen
-# 	if args["display"] > 0:
-# 		cv2.imshow("Frame", frame)
-# 		key = cv2.waitKey(1) & 0xFF
-# 	# update the FPS counter
-# 	# fps.update()
-# # stop the timer and display FPS information
-# # fps.stop()
-# # print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-# # print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# # do a bit of cleanup
-# cv2.destroyAllWindows()
-# vs.stop()
 
 ###########################################################################################################################3
 # Socket Create
@@ -120,10 +78,7 @@ while True:
 	client_socket,addr = server_socket.accept()
 	print('GOT CONNECTION FROM:',addr)
 	if client_socket:
-		# vid = cv2.VideoCapture(0)
-		
 		while(vs):
-			# img,frame = vid.read()
 			frame = vs.read()
 			frame = imutils.resize(frame,width=320,inter=cv2.INTER_LANCZOS4)
 			
@@ -133,11 +88,6 @@ while True:
 				# print(message)
 				continue
 			client_socket.sendall(message)
-			
-			# cv2.imshow('TRANSMITTING VIDEO',frame)
-			# key = cv2.waitKey(1) & 0xFF
-			# if key ==ord('q'):
-			# 	client_socket.close()	
 					
 # # Socket Accept
 # while True:
