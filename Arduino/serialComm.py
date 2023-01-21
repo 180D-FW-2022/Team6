1592b9ee1ba33074bd4d83e56299d0b52ff6aca0
Michael Buck <mrmabuck@gmail.com>
2023-01-21 12:06:21 -0800

    serial

--- Arduino/serialComm.py
+++ Arduino/serialComm.py
@@ -3,10 +3,11 @@ import serial
 import time
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
         ser.write(b"Hello from Raspberry Pi!\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
+
         time.sleep(1)
