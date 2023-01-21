51088d4ba30cdf5ff082786332aa7d0cd8c4f78a
Michael Buck <mrmabuck@gmail.com>
2023-01-21 10:57:30 -0800

    comm test

--- Integration/serverTest_revised.py
+++ Integration/serverTest_revised.py
@@ -110,10 +110,15 @@ try:
 					continue
 				client_socket.sendall(message)
 				from_client = ''
+				print('1')
 				while True:
+					print('2')
 					client_message = client_socket.recv(4096).decode()
+					print('3')
 					if not client_message: break
+					print('4')
 					from_client += client_message.decode('utf_8')
+					print('5')
 					print(from_client)
 				# from_client = client_socket.recv(4096).decode()
 				# print(from_client)						
