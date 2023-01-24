953f84deb1cad0f8e7e959d0edb73f89932100cd
Michael Buck <mrmabuck@gmail.com>
2023-01-23 16:36:42 -0800

    ip change

--- Integration/clientTest_revised.py
+++ Integration/clientTest_revised.py
@@ -40,7 +40,7 @@ f.close()
 
 # create socket
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '131.179.29.169' # paste your server ip address here
+host_ip = '169.232.126.22' # paste your server ip address here
 port = 9999
 client_socket.connect((host_ip,port)) # a tuple
 data = b""
