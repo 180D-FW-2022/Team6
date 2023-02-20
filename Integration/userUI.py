0f08583a76ade2bc72bfc89a110fbb8acd7793f2
Michael Buck <mrmabuck@gmail.com>
2023-02-19 21:42:51 -0800

    updates

--- Integration/userUI.py
+++ Integration/userUI.py
@@ -1,8 +1,8 @@
 
 
-videographer_ip = '164.67.233.31'
+videographer_ip = '128.97.51.195'
 
-remote_ip = '131.179.29.41'
+remote_ip = '169.232.126.235'
 
 # Don't call this temp_video, temp_video2, or temp_audio
 filename = 'Video' 
