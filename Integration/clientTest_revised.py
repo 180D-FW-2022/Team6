ab61c01cd037763a625e25afb491161976737651
Michael Buck <mrmabuck@gmail.com>
2023-02-20 19:37:04 -0800

    ts

--- Integration/clientTest_revised.py
+++ Integration/clientTest_revised.py
@@ -100,6 +100,7 @@ def frompi():
 
 	while True:
 		current_time = time.time()
+		cv2.waitKey()
 		while len(data) < payload_size:
 			packet = client_socket.recv(4*1024) # 4K
 			if not packet: break
@@ -114,7 +115,10 @@ def frompi():
 			data += client_socket.recv(4*1024)
 		frame_data = data[:msg_size]
 		data  = data[msg_size:]
-		frame = pickle.loads(frame_data)
+		try:
+			frame = pickle.loads(frame_data)
+		except:
+			continue
 		
 		# img,frame = vid.read()
 		
