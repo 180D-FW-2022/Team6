7e596e586135a17183f3d9b32212c7879f2e2767
jimiclic <69395593+jimiclic@users.noreply.github.com>
2023-02-22 17:05:22 -0800

    Update userUI.py

--- Integration/userUI.py
+++ Integration/userUI.py
@@ -5,7 +5,7 @@ videographer_ip = '131.179.29.201'
 remote_ip = '131.179.40.25'
 
 # Don't call this temp_video, temp_video2, or temp_audio
-filename = 'Video' 
+filename = 'team_switch_testing' 
 
 rate = 44100
 frames_per_buffer = 1024
